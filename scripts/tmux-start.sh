525028764d19db97074531aef6e561aed55a939a
lenohard <mathestics@gmail.com>
2018-08-07 11:39:57 +0800

    update vim tmux scripts

--- scripts/tmux-start.sh
+++ scripts/tmux-start.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 #git clone https://github.com/gpakosz/.tmux.git ~/
+if [ ! -n "$TMUX" ]; then
+    TMUX=~/.tmux
+fi
+
+if [  -d "$TMUX" ]; then
+    printf "Note: your ~/.tmux will be mv tu .zsh.bk\n"
+    mv ~/.tmux ~/.tmux.bk.$RANDOM
+fi
+
 mkdir ~/.tmux
-cp ../tmux.conf ~/.tmux/.tmux.conf
-ln -s  .tmux/.tmux.conf ~/.tmux.conf
+echo "mkdir ~/.tmux: ok!"
 
+if [ -h ~/.tmux.conf ]; then
+    echo "Note: your ~/.tmux.conf will be mv to .tmux.conf.bk"
+    mv ~/.tmux.conf ~/.tmux.conf.bk
+fi
+
+cp ../tmux/tmux.conf "$TMUX"/.tmux.conf && ln -s  "$TMUX"/.tmux.conf ~/.tmux.conf
+echo "creating link ~/.tmux.conf: ok!"
 cp ../tmux/tmux.conf.local ~/.tmux.conf.local
-
+echo "copying tmux.conf.local: ok!"
 cp ../tmux/plugins ~/.tmux/ -r
+echo "copying tmux/plugins/: ok!"
--- scripts/vim-start.sh
+++ scripts/vim-start.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
-git clone https://github.com/amix/vimrc.git .vim_runtime
+#    if [[ -d ~/.vim_runtime ]]; then
+#        echo "Note: previous .vim_runtime will be mvd to .vim_runtime.bk"
+#        mv ~/.vim_runtime ~/.vim_runtime.bk$RANDOM
+#    fi
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+
+if [[ ! -d ~/.vim_runtime ]]; then
+    git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+fi
+
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 cp  ../vim/my_configs.vim ~/.vim_runtime/ -f
+echo "copying my_configs: ok!"
 cp ../vim/my_plugins ~/.vim_runtime -rf
+echo "copying my_plugins: ok!"

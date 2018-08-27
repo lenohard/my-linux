0d4fba3554e503967eae36562c6b6b71e9c2b1f3
lenohard <mathestics@gmail.com>
2018-08-27 11:32:59 +0800

    change to link solution

--- scripts/tmux-start.sh
+++ scripts/tmux-start.sh
@@ -6,27 +6,28 @@ if [ ! -n "$TMUX" ]; then
     TMUX=~/.tmux
 fi
 
-if [  -d "$TMUX" ]; then
-    printf "Note: your ~/.tmux will be mv tu .zsh.bk\n"
-    mv ~/.tmux ~/.tmux.bk.$RANDOM
+if [[ -d "$TMUX" ]]; then
+    echo "You need to delete your previous ~/.tmux to go on"
+    exit
 fi
-
-mkdir ~/.tmux
-echo "mkdir ~/.tmux: ok!"
 
 if [ -h ~/.tmux.conf ]; then
     echo "Note: your ~/.tmux.conf will be mv to .tmux.conf.bk"
     mv ~/.tmux.conf ~/.tmux.conf.bk
 fi
-
-cp ../tmux/tmux.conf "$TMUX"/.tmux.conf && ln -s  "$TMUX"/.tmux.conf ~/.tmux.conf
+ln -s $(dirname $PWD)/tmux/tmux.conf ~/.tmux.conf
 echo "creating link ~/.tmux.conf: ok!"
-cp ../tmux/tmux.conf.local ~/.tmux.conf.local
+
+[[ -h ~/.tmux.conf.conf || -f ~/.tmux.conf.local ]] && mv ~/.tmux.conf.local ~/.tmux.conf.local.$RANDOM
+ln -s $(dirname $PWD)/tmux/tmux.conf.local ~/.tmux.conf.local
 echo "copying tmux.conf.local: ok!"
 #cp ../tmux/plugins ~/.tmux/ -r
 #echo "copying tmux/plugins/: ok!"
-mkdir -p ~/.tmux/plugin
-cd ~/.tmux/plugin
-git clone https://github.com/tmux-plugins/tpm.git
-git clone https://github.com/tmux-plugins/tmux-resurrect.git
+if [ ! -d ~/.tmux/plugin ]; then
+    mkdir -p ~/.tmux/plugin
+    cd ~/.tmux/plugin
+    git clone https://github.com/tmux-plugins/tpm.git
+    git clone https://github.com/tmux-plugins/tmux-resurrect.git
+fi
+ln -s $(dirname $DIR)/tmux/resurrect ~/.tmux/resurrect
 cd $cwd

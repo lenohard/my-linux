d6c9d0a4eb3f5292883b84cad4e2d0fa0653086a
lenohard <mathestics@gmail.com>
2018-09-18 16:49:03 +0800

    "."

--- kit/Git.sh
+++ kit/Git.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-cur_dir='$(pwd)'
+cur_dir="$(pwd)"
 cd $(git rev-parse --show-toplevel)
 
 git status
-read -p  "Continue? [y]|n" -n 1 -r
+read -p  "Continue? (y|n)" -n 1 -r
 echo
 
-if  [[ $REPLY =~ ^[Yy]$ ]]
+if  [[ $REPLY =~ ^[Yy]$ || -z $REPLY ]]
 then
     git add .
-    echo "message: " -n
+    echo -n "Commit Message: "
     read m
     git commit -m "$m"
     git push
 fi
+
+cd "$cur_dir"

bb5f7b1e46fbb9cecd2186db329f16d9781bb5ba
lenohard <mathestics@gmail.com>
2018-09-22 02:33:19 +0000

    "."

--- scripts/vim-start.sh
+++ scripts/vim-start.sh
@@ -15,6 +15,7 @@ fi
 
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 [[  -e ~/.vim_runtime/autoload/plug.vim ]] || cp ../vim/plug.vim ~/.vim_runtime/autoload/
+[[ -e ~/.vim_runtime/.git/info/exclude  || -h ~/.vim_runtime/.git/info/exclude ]] && rm ~/.vim_runtime/.git/info/exclude && ln $ROOT/vim/exclude ~/.vim_runtime/.git/info/exclude -s
 
 [[ -e ~/.vim_runtime/my_configs.vim || -h ~/.vim_runtime/my_configs.vim ]] && rm ~/.vim_runtime/my_configs.vim
 ln  $ROOT/vim/my_configs.vim ~/.vim_runtime/ -s

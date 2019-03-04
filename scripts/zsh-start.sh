f9370a51dbdb456f0dcc775f556e97038cf2e7ad
lenohard <mathestics@gmail.com>
2019-03-04 14:19:00 +0800

    update

--- scripts/zsh-start.sh
+++ scripts/zsh-start.sh
@@ -6,9 +6,17 @@ echo root dir is \"$ROOT\"
 bash $ROOT/zsh/install.sh
 
 if [[ -e ~/.zshrc || -h ~/.zshrc ]]; then
-    echo "the existing zshrc will be mv to .zshrc_bk"
-    mv ~/.zshrc ~/.zshrc_bk
-    mv ~/.inputrc ~/.inputrc_bk
+	echo "the existing zshrc will be mv to .zshrc_bk"
+	mv ~/.zshrc ~/.zshrc_bk
 fi
 ln -s $ROOT/zsh/zshrc ~/.zshrc
+if [[ -e ~/.inputrc || -h ~/.inputrc ]]; then
+	echo "the existing zshrc will be mv to .zshrc_bk"
+	mv ~/.inputrc ~/.inputrc_bk
+fi
 ln -s $ROOT/zsh/inputrc ~/.inputrc
+if [[ -e ~/.ssh || -h ~/.ssh ]]; then
+	echo "the existing zshrc will be mv to .zshrc_bk"
+	mv ~/.ssh ~/.ssh_bk
+fi
+ln -s $ROOT/zsh/ssh ~/.ssh

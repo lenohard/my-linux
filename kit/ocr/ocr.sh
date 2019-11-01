6f4ca654d38d168e4a4f6763dc7160618567fb3b
carlleonhard <mathestics@gmail.com>
2019-11-01 15:57:30 +0800

    new branche for mac right now

--- kit/ocr/ocr.sh
+++ kit/ocr/ocr.sh
@@ -1,26 +1,22 @@
-#!/bin/bash
-read -p "do you want to know the workflow of ocr_suite?" -n 1 -r
-if [[ $REPLY =~ [yY] ]];
-then
-    echo ./readme
-fi
-set -e
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: OCR.sh
+#
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Carlleonhard
+#  ORGANIZATION:
+#       CREATED: 11/21/2018 19:28
+#      REVISION:  0
+#===============================================================================
 
-if [[ $# -ne 3 ]]; then
-    echo " ocr fileanme first last (please ;\) "
-    exit
-fi
-
-f=$2
-l=$3
-[ ${1#*.} = 'djvu' ] &&  dj2pdf.sh $1
-file="${1/.djvu/.pdf}"
-echo $file
-sleep 3
-while [[ ! $l -lt $f ]]
-do
-    pdftoppm "$file" -png  -f $f -singlefile image && tesseract image.png output$f --oem 3 --psm 6
-    f=$((f+1))
-done
-cat output*  | tee result.txt
-rm output* image.png
+set -o nounset                              # Treat unset variables as an error
+ocr_generate content > tmp.awk
+./tmp.awk result.txt > tmp
+ocr_final.sh "$1"

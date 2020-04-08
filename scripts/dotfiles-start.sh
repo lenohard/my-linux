4b09e5624868314176a17f559ba90d6812fa0d63
lenohard <mathestics@gmail.com>
2020-04-09 01:01:10 +0800

    wdh

--- scripts/dotfiles-start.sh
+++ scripts/dotfiles-start.sh
@@ -10,14 +10,30 @@ if [[ -e ~/.inputrc || -h ~/.inputrc ]]; then
 fi
 ln -s $ROOT/zsh/inputrc ~/.inputrc
 
-if [[ -e ~/.ssh || -h ~/.ssh ]]; then
-	echo "the existing .ssh will be mv to .ssh_bK"
-	mv ~/.ssh ~/.ssh_bk
-fi
-ln -s $ROOT/zsh/ssh ~/.ssh
+# if [[ -e ~/.ssh || -h ~/.ssh ]]; then
+# 	echo "the existing .ssh will be mv to .ssh_bK"
+# 	mv ~/.ssh ~/.ssh_bk
+# fi
+# ln -s $ROOT/zsh/ssh ~/.ssh
 
 if [[ -e ~/.racketrc || -h ~/.racketrc ]]; then
 	echo "the existing racketrc will be mv to .zshrc_bk"
     mv ~/.racketrc ~/.racketrc_bk
 fi
 ln -s $ROOT/zsh/racketrc ~/.racketrc
+
+
+if [[ -e ~/.ghci || -h ~/.ghci ]]; then
+	echo "the existing ghci will be mv to .zshrc_bk"
+    mv ~/.ghci ~/.ghci_b
+fi
+ln -s $ROOT/zsh/ghci ~/.ghci
+
+
+if [[ -e ~/.gitconfig || -h ~/.ghci ]]; then
+	echo "the existing gitconfig will be mv to .zshrc_bk"
+    mv ~/.gitconfig ~/.ghci_b
+fi
+ln -s $ROOT/zsh/gitconfig ~/.ghci
+
+
